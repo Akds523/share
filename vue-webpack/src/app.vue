59e5d708444d893d0351840ace17cc6e8f121d57
Akds523 <ge_qinyukun@163.com>
2018-11-19 15:05:44 +0800

    webpack1.0_vue

---
+++ vue-webpack/src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="test">{{text}}</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      text: 'Abcdedfrffgdfg'
+    }
+  }
+}
+</script>
+
+<style>
+ #test{
+  color:#00a6e3;
+ }
+</style>
